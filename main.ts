c24116542dd56d0087b27a7fb8a53b413ea30f18
waseem4100 <waseemahmed4100@gmail.com>
2024-07-02 12:24:17 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,31 @@
+
+
+import inquirer from "inquirer";
+
+async function BMI() {
+  let answer = await inquirer.prompt([
+    {
+      type: "input",
+      name: "weight",
+      message: "What is your weight? (in KG)",
+    },
+    {
+      type: "input",
+      name: "height",
+      message: "What is your height? (in Metre)",
+    },
+  ]).then((answer) => {
+    let weight = parseFloat(answer.weight);
+    let height = parseFloat(answer.height);
+
+    function calculateBMI(height: number, weight: number) {
+      return weight / (height * height);
+    }
+
+    let BMIValue = calculateBMI(height, weight);
+    console.log(`Your BMI ratio is ${BMIValue}`);
+    return BMIValue;
+  });
+}
+
+BMI();
